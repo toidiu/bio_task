c3677f312ef73fe33bd77450d17ecdcaa935a520
toidiu <apoorv@toidiu.com>
2019-07-12 11:12:27 +0200

    sort img

--- website/src/components/task-view/TaskView.vue
+++ website/src/components/task-view/TaskView.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <div>debug: sort={{ currentSort }}, dir={{ currentSortDir }}</div>
     <table class="table">
       <tr>
         <template v-for="([colName, colKey], idx) in columns">
-          <th>{{ colName }}</th>
+          <th @click="sort(colKey)">
+            <div class="sort-wrapper">
+              <img class="sort-img" src="./../../../static/images/sort.svg" />
+              <div class="sort-text">
+                {{ colName }}
+              </div>
+            </div>
+          </th>
         </template>
       </tr>
 
-      <template v-for="(task, tidx) in tasksState">
+      <template v-for="(task, tidx) in sortedTasksState">
         <tr>
           <template v-for="([colName, colKey], idx) in columns">
             <td>
@@ -37,14 +45,45 @@ export default Vue.extend({
         ["ProjectId", "projectId"],
         ["Deadline", "deadlineDate"],
         ["MemberId", "memberId"]
-      ]
+      ],
+      currentSort: "deadlineDate",
+      currentSortDir: "asc"
     };
   },
-  methods: {}
+  methods: {
+    sort: function(s) {
+      //if s == current sort, reverse
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+      }
+      this.currentSort = s;
+    }
+  },
+  computed: {
+    sortedTasksState: function() {
+      return this.tasksState.sort((a, b) => {
+        let modifier = 1;
+        if (this.currentSortDir === "desc") modifier = -1;
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+        return 0;
+      });
+    }
+  }
 });
 </script>
 
 <style lang="scss" scoped>
+div {
+  display: ruby;
+}
+.sort-img {
+  width: 14px;
+  height: auto;
+  vertical-align: middle;
+}
+.sort-text {
+}
 table {
   table-layout: unset;
 }
